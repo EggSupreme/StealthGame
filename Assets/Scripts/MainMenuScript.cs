7f04f01ef98edf7fc770d012dab4bb1dfff8b843
EggSupreme <63319039+EggSupreme@users.noreply.github.com>
2021-03-09 17:18:51 +0000

    Main Menu Update
    Added a Main Menu Scene with two canvases, one for the Main Menu and one for an Options Menu.
    
    Added an audio player for eventual music that can carry between scenes, as well as a volume mixer and separate volume sliders for Master Volume, Music Volume and SFX Volume. May want to add Voice as a separate volume at some point.
    
    REMEMBER to add all the scenes in the Build Settings menu in unity so that the menu script can actually access them for switching, currently the start button takes to my test scene just to see if it was working.

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public GameObject optionsScreen;
+    public GameObject mainMenu;
+
+    void Start()
+    {
+        optionsScreen = GameObject.Find("OptionsCanvas");
+        optionsScreen.SetActive(false);
+
+        mainMenu = GameObject.Find("MainMenuCanvas");
+        mainMenu.SetActive(true);
+    }
+
+   public void startButton()
+    { SceneManager.LoadScene("George_TestScene"); }
+
+    public void menuButton()
+    { SceneManager.LoadScene("MainMenuScene"); }
+
+    public void optionsButton()
+    {
+        if (!optionsScreen.activeInHierarchy)
+        {
+            optionsScreen.SetActive(true);
+            mainMenu.SetActive(false);
+        }
+        else
+        {
+            optionsScreen.SetActive(false);
+            mainMenu.SetActive(true);
+        }
+    }
+
+    public void quitButton()
+    { Application.Quit(); }
+
+}

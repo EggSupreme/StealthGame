66dec851747530c96dbcff75996c87b981988d57
EggSupreme <63319039+EggSupreme@users.noreply.github.com>
2021-04-23 18:00:48 +0100

    Saving prep
    made data scripts for location and tool ammo, need access to tool ammo itself to continue though

---
+++ Assets/Scripts/ToolData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ToolData
+{
+    public int rockAmmo;
+    public int dartAmmo;
+    public int bowAmmo;
+
+    public ToolData(Tools_Abilities toolbelt)
+    {
+        rockAmmo = 1;
+        dartAmmo = 1;
+        bowAmmo = 1;
+    
+    }
+
+
+}
